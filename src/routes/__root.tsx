4b228e99d83be1677866d5cb94076972f5b85188
akshayks13 <akshayks1005@gmail.com>
2026-01-20 13:39:21 +0530

    chore : added initial setup commit

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,11 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Toaster } from 'react-hot-toast'
 
 import Header from '../components/Header'
 
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+import TanStackQueryDevtools from '../providers/tanstack-query-devtools'
 
 import type { QueryClient } from '@tanstack/react-query'
 
@@ -17,6 +18,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     <>
       <Header />
       <Outlet />
+      <Toaster position="top-right" />
       <TanStackDevtools
         config={{
           position: 'bottom-right',
