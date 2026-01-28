ef32eb3259cb200a30994eb863288331e0d3cb5f
Karthik Saiharsh <ks.the.visionary@gmail.com>
2026-01-28 20:26:19 +0530

    Title Sponsor content update

--- src/routes/sponsors/index.tsx
+++ src/routes/sponsors/index.tsx
@@ -5,81 +5,117 @@ import Noise from "@/components/sponsors/Noise";
 import "./style.css";
 
 export const Route = createFileRoute("/sponsors/")({
-	component: SponsorsPage,
+  component: SponsorsPage,
 });
 
 function SponsorsPage() {
-	return (
-		<section className="w-screen h-screen bg-black select-none">
-			<div className="absolute overflow-hidden w-full h-full bg-black/30 flex flex-col items-center backdrop-blur-[5px] z-10">
-				<Noise
-					patternSize={50}
-					patternScaleX={10}
-					patternScaleY={10}
-					patternRefreshInterval={2}
-					patternAlpha={25}
-				/>
+  return (
+    <section className="w-screen h-screen bg-black select-none overflow-hidden">
+      <div className="absolute overflow-y-scroll w-full h-screen bg-black/30 flex flex-col items-center backdrop-blur-[5px] z-10">
+        <div className="absolute inset-0 w-full h-[150vh] pointer-events-none">
+          <Noise
+            patternSize={50}
+            patternScaleX={10}
+            patternScaleY={10}
+            patternRefreshInterval={2}
+            patternAlpha={25}
+          />
+        </div>
 
-				<motion.h1
-					initial={{ translateY: 120, opacity: 0 }}
-					animate={{ translateY: 0, opacity: 1 }}
-					transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
-					className="text-[#f4d03e] text-8xl font-jersey text-shadow-[4px_4px_0px_#7b3aec] z-20 mb-5"
-				>
-					Our Sponsors
-				</motion.h1>
+        <motion.h1
+          initial={{ translateY: 120, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: "easeInOut", delay: 0.5 }}
+          className="text-[#f4d03e] text-8xl font-jersey text-shadow-[4px_4px_0px_#7b3aec] z-20 mb-5 max-sm:text-7xl"
+        >
+          Our Sponsors
+        </motion.h1>
 
-				{/* Sponsors Cards */}
+        {/* Sponsors Cards */}
 
-				{/* Title Sponsors */}
-				<motion.p
-					initial={{ translateY: 120, opacity: 0 }}
-					animate={{ translateY: 0, opacity: 1 }}
-					transition={{ duration: 1, ease: "easeInOut", delay: 1 }}
-					className="text-[#f4d03e] text-5xl font-jersey text-shadow-[2px_2px_0px_#7b3aec] z-20"
-				>
-					Title Sponsors
-				</motion.p>
+        {/* Title Sponsors */}
+        <motion.p
+          initial={{ translateY: 120, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: "easeInOut", delay: 1 }}
+          className="text-[#33EBFF] text-5xl font-jersey text-shadow-[2px_2px_0px_#7b3aec] z-20 mt-10"
+        >
+          Title Sponsor - BNY
+        </motion.p>
 
-				<div className="w-full h-full px-10 flex flex-1 flex-wrap justify-center gap-10">
-					{/* Title Sponsors */}
-					<Card
-						name="BNY"
-						img="sponsors-page/bny.jpeg"
-						extraStyling="w-1/2"
-						appearDelay={1.2}
-					/>
-				</div>
+        <motion.div
+          initial={{ translateY: 120, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: "easeInOut", delay: 1.2 }}
+          className="relative w-3/4 max-w-4xl my-8"
+        >
+          <img
+            src="sponsors-page/card-long.png"
+            alt=""
+            className="w-full h-auto"
+          />
+          <div className="absolute inset-0 flex items-center gap-8 px-15 py-8">
+            <img
+              src="sponsors-page/bny.jpeg"
+              alt="BNY Logo"
+              className="w-50 h-auto object-contain rounded-xl"
+            />
+            <div className="flex flex-col overflow-scroll h-[90%] -translate-y-3 gap-5">
+              <p className="text-white text-lg font-jersey leading-relaxed">
+                BNY is a global financial services platforms company and the
+                world's largest custodian bank. Founded in 1784 by Alexender
+                Hamilton. BNY supports how capital moves, settles, and stays
+                secure across markets. At its core, BNY provides the
+                infrastructure that focuses on safekeeping assets, processing
+                transactions, and supporting clients across the full investment
+                lifecycle. The firm focuses on serving clients well, taking
+                responsibility, learning continuously, and working together as
+                one team
+              </p>
 
-				<motion.p
-					initial={{ translateY: 120, opacity: 0 }}
-					animate={{ translateY: 0, opacity: 1 }}
-					transition={{ duration: 1, ease: "easeInOut", delay: 1.4 }}
-					className="text-[#f4d03e] text-5xl font-jersey text-shadow-[2px_2px_0px_#7b3aec] z-20"
-				>
-					Other Sponsors
-				</motion.p>
+              <p className="text-white text-lg font-jersey leading-relaxed">
+                Amrita School of Business shares a strong and growing
+                association with BNY. Over the years, BNY has actively supported
+                Amrita students through meaningful internship opportunities,
+                helping them gain real-world exposure to global financial
+                services. We are grateful for BNY's continued support in
+                sponsoring the flagship management fest, Pragati'25 and
+                Pragati'26, reinforcing their commitment to academic
+                collaboration and talent development
+              </p>
+            </div>
+          </div>
+        </motion.div>
 
-				<div className="w-full h-full px-10 flex flex-1 flex-wrap justify-center gap-10">
-					{/* Other Sponsors */}
-					<Card
-						name="Banconus"
-						img="sponsors-page/banconus.jpeg"
-						extraStyling="w-1/3"
-						appearDelay={1.6}
-					/>
-				</div>
-			</div>
+        <motion.p
+          initial={{ translateY: 120, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: "easeInOut", delay: 1.4 }}
+          className="text-[#E6E6FA] text-5xl font-jersey text-shadow-[2px_2px_0px_#7b3aec] z-20 mt-10"
+        >
+          Other Sponsors
+        </motion.p>
 
-			<video
-				className="absolute z-0 w-auto min-w-full min-h-full max-w-none"
-				autoPlay
-				loop
-				muted
-			>
-				<source src="sponsors-page/bg-night.webm" type="video/webm" />
-				Video Playback not supported!
-			</video>
-		</section>
-	);
+        <div className="w-full px-10 py-8 flex flex-wrap justify-center items-start gap-8 pb-20">
+          {/* Other Sponsors */}
+          <Card
+            name="Banconus"
+            img="sponsors-page/banconus.jpeg"
+            extraStyling="w-1/3"
+            appearDelay={1.6}
+          />
+        </div>
+      </div>
+
+      <video
+        className="absolute top-0 left-0 z-0 w-screen h-[110vh] min-w-full min-h-full max-w-none"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="sponsors-page/bg-night.webm" type="video/webm" />
+        Video Playback not supported!
+      </video>
+    </section>
+  );
 }
